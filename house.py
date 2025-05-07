f3313cc52a4dd8dc8c64b11c4476ea8ecc50cb12
Eduard Kurlov <edgreen.n.b@gmail.com>
2025-05-07 12:46:44 +0300

    Заготовка программы для рисования домика

--- house.py
+++ house.py
@@ -1,3 +1,19 @@
-def house():
-	pass
+import pygame
 
+pygame.init()
+weidth, height = screen_size = (600, 400)
+screen = pygame.display.set_mode(screen_size)
+
+# Здесь нужно рисовать
+# draw_picture(screen)
+pygame.display.update()
+
+work_flag = True
+while work_flag:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			
+			work_flag = False
+
+print('Program is over')
