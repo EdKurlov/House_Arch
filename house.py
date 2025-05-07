4109d340576f88be8a3b878a8a0ff68038b4b0ef
Eduard Kurlov <edgreen.n.b@gmail.com>
2025-05-07 16:58:57 +0300

    Сделал декомпозицию, выделив функцию 'draw_picture'

--- house.py
+++ house.py
@@ -1,11 +1,25 @@
 import pygame
 
+def draw_picture(screen, x, y, weidth, height):
+	"""
+	Рисует картинку с домиком на фоне травы и неба.
+
+	:param screen: дисплей pygame, на котором будет изображение
+	:param x: левая координата прямоугольника с рисунком
+	:param y: верхняя координата прямоугольника с рисунком
+	:param weidth: ширина прямоугольника
+	:param height: высота прямоугольника
+	"""
+	print("as if I were drawing a picture", x, y, weidth, height)
+
 pygame.init()
 weidth, height = screen_size = (600, 400)
 screen = pygame.display.set_mode(screen_size)
 
 # Здесь нужно рисовать
-# draw_picture(screen)
+draw_picture(screen, 0, 0, weidth, height)
+
+
 pygame.display.update()
 
 work_flag = True
